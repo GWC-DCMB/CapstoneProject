b4ef7fe976501031852e89929b3f6b525d925b1b
brissend <brissend725@gmail.com>
2023-01-06 11:28:22 -0500

    cleaning script edit

--- datasets/world_happiness/world_happiness_cleaning.R
+++ datasets/world_happiness/world_happiness_cleaning.R
@@ -1,12 +1,16 @@
 library(tidyverse)
 setwd('~/Dropbox (University of Michigan)/Code/GirlsWhoCode/CapstoneProject/datasets/world_happiness/')
 
+# ==========
 # load data
+# ==========
 df15 = read_csv('2015.csv')
 df16 = read_csv('2016.csv')
 df17 = read_csv('2017.csv')
 
+# ======================================
 # standardize column names across years
+# ======================================
 colnames(df15)
 colnames(df16)
 colnames(df17)
@@ -37,12 +41,16 @@ df17 = df17 %>%
          Health.Life.Expectancy = Health..Life.Expectancy.,
          Trust.Government.Corruption = Trust..Government.Corruption.)
 
-
+# =====================================================
+# compute happiness score confidence interval for 2015
+# =====================================================
 # 2015 dataset reports standard error instead of lower and upper 95% confidence interval. Compute confidence interval from standard error.
 df15$Lower.Confidence.Interval = df15$Happiness.Score + (df15$Standard.Error * qnorm(0.025))
 df15$Upper.Confidence.Interval = df15$Happiness.Score + (df15$Standard.Error * qnorm(0.975))
 
-# Merge datasets
+# ========================
+# Merge data across years
+# ========================
 # add year variable
 df15$Year = 2015
 df16$Year = 2016
@@ -52,7 +60,15 @@ df17$Year = 2017
 df = full_join(df15,df16)
 df = full_join(df,df17)
 
-# drop columns that don't appear in all datasets
-df = df %>% select(-Region,-Standard.Error)
+# drop standard error column that only appears in the 2015 dataset
+df = df %>% select(-Standard.Error)
+
+# reorder columns
+df = df %>% relocate(Year,.before = Country)
+df = df %>% relocate(Lower.Confidence.Interval, .after = Happiness.Score)
+df = df %>% relocate(Upper.Confidence.Interval, .after = Lower.Confidence.Interval)
+
+# save
+write_csv(df,'world_happiness.csv',col_names = TRUE)
          
          
